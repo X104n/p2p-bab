0bce3b3f0bce3abcac9bb106b7f2d705d4a2a3e3
Lars Bysheim <lars.bysheim@proton.me>
2025-03-29 17:45:15 +0100

    map finish?

--- src/app/(protected)/map/page.tsx
+++ src/app/(protected)/map/page.tsx
@@ -2,40 +2,63 @@
 import { useEffect, useId, useState, useRef } from "react";
 import "./style.css";
 
-const initial_time = 600000;
-const step_size = 10;
+const step_size = 1000;
 
-const positions = [
-  { x: 50, y: 50 },
-  { x: 50, y: 10 },
-  { x: 10, y: 80 },
-];
+
+interface MapData {
+    hate: string,
+    feedback: string,
+    coordinates: {x:number,y:number}
+      
+}
 
 export default function Grapher() {
-  const [nxtUpdate, setNxtUpdate] = useState<number>(initial_time);
-  const [globalX, setGlobalX] = useState<number>(0);
-  const [show, setShow] = useState<boolean>(false);
-  const containerRef = useRef<HTMLDivElement | null>(null); // Explicitly type the ref
+    const [mapdata, setMapdata] = useState<MapData>({
+        hate: "",
+        feedback: "Start walking!",
+        coordinates:{x:50,y:50}
+    });
 
-  // Effect to update the width and height of the window
-  useEffect(() => {
+    const [nxtUpdate, setNxtUpdate] = useState<number>(step_size * 10);
+    const [globalpos, setGlobalpos] = useState<{x:number, y:number}>({x:0,y:0});
+    const [show, setShow] = useState<boolean>(false);
+    const [theFeedback, setTheFeedback] = useState<string>("Start walking!");
+    const [positions, setPositions] = useState<{x:number,y:number}[]>([{x:50,y:50}]);
+    const containerRef = useRef<HTMLDivElement | null>(null);
+
+    const fetchMapData = async () => {
+        try {
+        const response = await fetch("/api/map");
+        
+        if (!response.ok) {
+            throw new Error("Failed to fetch profile data");
+        }
+        const data = await response.json();
+        console.log(data);
+        setMapdata(() => data);
+        } catch (err) {
+            console.error("Error fetching map data:", err);
+        } 
+    };
+
+    const updateDimensions = () => {
+        if (!containerRef.current) return;
+
+        setGlobalpos({
+            x:  (mapdata.coordinates.x / 100) * containerRef.current.offsetWidth + containerRef.current.offsetLeft + 30,
+            y:  (mapdata.coordinates.y / 100) * containerRef.current.offsetHeight + containerRef.current.offsetTop - 60,
+        })
+    };
+
+    // Effect to update the width and height of the window
+    useEffect(() => {
+        fetchMapData();
         // Function to update the dimensions
-        const updateDimensions = () => {
-            const containerWidth = containerRef.current ? containerRef.current.offsetWidth : 0;
-            const containerLeft = containerRef.current ? containerRef.current.offsetLeft : 0;
-            setGlobalX(() => positions.length > 0  ? (positions[positions.length - 1].x / 100) * containerWidth + containerLeft + 30: 0);
-        };
 
         const int = setInterval(() => {
             setNxtUpdate((prev) => prev - step_size);
-          }, 10);
+        }, step_size);
 
-        const int2 = setInterval(() => {
-            setShow(() => true);
-            setTimeout(() => {
-                setShow(() => false);
-            }, 5000);
-        }, 10000);
 
         updateDimensions();
         window.addEventListener('resize', updateDimensions);
@@ -44,65 +67,79 @@ export default function Grapher() {
         return () => {
             window.removeEventListener('resize', updateDimensions);
             clearInterval(int);
-            clearInterval(int2);
         };
     }, []);
 
 
+    useEffect(() => {
+        if (nxtUpdate > 0) return;
+        fetchMapData().then(() => {
+            setPositions((prev) => [...prev, mapdata.coordinates]);
+            setTheFeedback(prev => mapdata.feedback);
+            setNxtUpdate(step_size * 10);
+            updateDimensions();
+            setTimeout(() => {
+                setShow(() => true);
+                setTimeout(() => {
+                    setShow(() => false);
+                }, step_size * 3);
+            }, step_size );
+        });
+    }, [nxtUpdate])
 
-  return (
-    <div className="container">
-      <div className="body">
-        <div className="direction_container">
-            <p style={{transform:'rotate(-90deg)'}}>NORTH</p>
+    return (
+        <div className="container">
+        <div className="body">
+            <div className="direction_container">
+                <p style={{transform:'rotate(-90deg)'}}>NORTH</p>
+            </div>
+            <div className="map_container" ref={containerRef}>
+                <svg width="100%" height="100%" viewBox="0 0 100% 100%">
+                    {positions.map((pos, i) => {
+                        if (i == positions.length - 1) return;
+                        return (
+                        <line
+                            key={`${i}-${pos.x}-${pos.y}`}
+                            strokeWidth={1}
+                            stroke="black"
+                            x1={pos.x + "%"}
+                            x2={positions[(i + 1) % positions.length].x + "%"}
+                            y1={pos.y + "%"}
+                            y2={positions[(i + 1) % positions.length].y + "%"}
+                        ></line>
+                        );
+                    })}
+                    {positions.map((pos,i) => (
+                        <circle
+                            key={`${i}l${pos.x}-${pos.y}`}
+                            fill="black"
+                            r={2}
+                            cx={pos.x + "%"}
+                            cy={pos.y + "%"}
+                        ></circle>
+                    ))}
+                        {<image
+                            href="/drunk.gif" // Path to your image in the public directory
+                            x={positions[positions.length - 1].x - 5 + "%"}
+                            y={positions[positions.length - 1].y - 5 + "%"}
+                            width="10%"
+                            height="10%"
+                        />}
+                </svg>
+            </div>
+            <div className="direction_container">
+                    <p style={{transform:'rotate(90deg)'}}>SOUTH</p>
+            </div>
+
+            <div className="chat_bubble" hidden={!show} style={{
+                left: globalpos.x + 'px', 
+                top: globalpos.y + 'px'}}> {mapdata.hate} </div>       
         </div>
-        <div className="map_container" ref={containerRef}>
-            <svg width="100%" height="100%" viewBox="0 0 100% 100%">
-                {positions.map((pos, i) => {
-                    if (i == positions.length - 1) return;
-                    return (
-                    <line
-                        key={useId()}
-                        strokeWidth={1}
-                        stroke="black"
-                        x1={pos.x + "%"}
-                        x2={positions[(i + 1) % positions.length].x + "%"}
-                        y1={pos.y + "%"}
-                        y2={positions[(i + 1) % positions.length].y + "%"}
-                    ></line>
-                    );
-                })}
-                {positions.map((pos) => (
-                    <circle
-                    key={useId()}
-                    fill="black"
-                    r={2}
-                    cx={pos.x + "%"}
-                    cy={pos.y + "%"}
-                    ></circle>
-                ))}
-                    {<image
-                        href="/drunk.gif" // Path to your image in the public directory
-                        x={positions[positions.length - 1].x - 5 + "%"}
-                        y={positions[positions.length - 1].y - 5 + "%"}
-                        width="10%"
-                        height="10%"
-                    />}
-            </svg>
+
+        <div className="header" suppressHydrationWarning>
+            <p className="status_text">{theFeedback}</p>
+            <p className="small_text">I’ll check again in {nxtUpdate/1000} seconds..</p>
         </div>
-        <div className="direction_container">
-                <p style={{transform:'rotate(90deg)'}}>SOUTH</p>
-         </div>
-
-        <div className="chat_bubble" hidden={!show} style={{
-            left: globalX + 'px', 
-            bottom: 35 +  positions[positions.length - 1].y * 0.1 + "%"}}> user_123 just got warmer... </div>       
-      </div>
-
-      <div className="header" suppressHydrationWarning>
-        <p className="status_text">I would'nt walk in that direction...</p>
-        <p className="small_text">I’ll check again in {nxtUpdate} ms</p>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
