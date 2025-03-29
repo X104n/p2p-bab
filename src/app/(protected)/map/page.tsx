c27c135af8a46ddd932dcfaecf1bdb3260691765
Lars Bysheim <lars.bysheim@proton.me>
2025-03-29 15:53:07 +0100

    cleand up the style of the map page

--- src/app/(protected)/map/page.tsx
+++ src/app/(protected)/map/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import Image from "next/image";
-import { useEffect, useId, useState } from "react";
+import { useEffect, useId, useState, useRef } from "react";
 import "./style.css";
 
 const initial_time = 600000;
@@ -14,69 +13,95 @@ const positions = [
 
 export default function Grapher() {
   const [nxtUpdate, setNxtUpdate] = useState<number>(initial_time);
-  const [prvUpdate, setPrvUpdate] = useState<number>(Date.now());
-  const [state, setState] = useState<string>(
-    "I would'nt walk in that direction...",
-  );
+  const [globalX, setGlobalX] = useState<number>(0);
+  const [show, setShow] = useState<boolean>(false);
+  const containerRef = useRef<HTMLDivElement | null>(null); // Explicitly type the ref
 
+  // Effect to update the width and height of the window
   useEffect(() => {
-    const int = setInterval(() => {
-      setNxtUpdate((prev) => {
-        if (prev - step_size <= 0) {
-          setPrvUpdate((prev) => Date.now());
-          return initial_time;
-        }
-        return prev - step_size;
-      });
-    }, 10);
-  }, []);
+        // Function to update the dimensions
+        const updateDimensions = () => {
+            const containerWidth = containerRef.current ? containerRef.current.offsetWidth : 0;
+            const containerLeft = containerRef.current ? containerRef.current.offsetLeft : 0;
+            setGlobalX(() => positions.length > 0  ? (positions[positions.length - 1].x / 100) * containerWidth + containerLeft + 30: 0);
+        };
+
+        const int = setInterval(() => {
+            setNxtUpdate((prev) => prev - step_size);
+          }, 10);
+
+        const int2 = setInterval(() => {
+            setShow(() => true);
+            setTimeout(() => {
+                setShow(() => false);
+            }, 5000);
+        }, 10000);
+
+        updateDimensions();
+        window.addEventListener('resize', updateDimensions);
+
+        // Cleanup function to remove the event listener
+        return () => {
+            window.removeEventListener('resize', updateDimensions);
+            clearInterval(int);
+            clearInterval(int2);
+        };
+    }, []);
+
+
 
   return (
     <div className="container">
+      <div className="body">
+        <div className="direction_container">
+            <p style={{transform:'rotate(-90deg)'}}>NORTH</p>
+        </div>
+        <div className="map_container" ref={containerRef}>
+            <svg width="100%" height="100%" viewBox="0 0 100% 100%">
+                {positions.map((pos, i) => {
+                    if (i == positions.length - 1) return;
+                    return (
+                    <line
+                        key={useId()}
+                        strokeWidth={1}
+                        stroke="black"
+                        x1={pos.x + "%"}
+                        x2={positions[(i + 1) % positions.length].x + "%"}
+                        y1={pos.y + "%"}
+                        y2={positions[(i + 1) % positions.length].y + "%"}
+                    ></line>
+                    );
+                })}
+                {positions.map((pos) => (
+                    <circle
+                    key={useId()}
+                    fill="black"
+                    r={2}
+                    cx={pos.x + "%"}
+                    cy={pos.y + "%"}
+                    ></circle>
+                ))}
+                    {<image
+                        href="/drunk.gif" // Path to your image in the public directory
+                        x={positions[positions.length - 1].x - 5 + "%"}
+                        y={positions[positions.length - 1].y - 5 + "%"}
+                        width="10%"
+                        height="10%"
+                    />}
+            </svg>
+        </div>
+        <div className="direction_container">
+                <p style={{transform:'rotate(90deg)'}}>SOUTH</p>
+         </div>
+
+        <div className="chat_bubble" hidden={!show} style={{
+            left: globalX + 'px', 
+            bottom: 35 +  positions[positions.length - 1].y * 0.1 + "%"}}> user_123 just got warmer... </div>       
+      </div>
+
       <div className="header" suppressHydrationWarning>
-        {prvUpdate}
         <p className="status_text">I would'nt walk in that direction...</p>
         <p className="small_text">I’ll check again in {nxtUpdate} ms</p>
-      </div>
-      <div className="body">
-        <svg width="100%" height="100%" viewBox="0 0 100% 100%">
-          <image
-            href="/map.jpeg" // Path to your image in the public directory
-            x="0"
-            y="0"
-            width="100%"
-            height="100%"
-            preserveAspectRatio="xMidYMid slice" // Adjusts how the image is scaled
-          />
-          {positions.map((pos, i) => {
-            if (i == positions.length - 1) return;
-            return (
-              <line
-                key={useId()}
-                strokeWidth={1}
-                stroke="red"
-                x1={pos.x + "%"}
-                x2={positions[(i + 1) % positions.length].x + "%"}
-                y1={pos.y + "%"}
-                y2={positions[(i + 1) % positions.length].y + "%"}
-              ></line>
-            );
-          })}
-          {positions.map((pos) => (
-            <circle
-              key={useId()}
-              fill="red"
-              r={2}
-              cx={pos.x + "%"}
-              cy={pos.y + "%"}
-            ></circle>
-          ))}
-        </svg>
-      </div>
-      <div className="footer">
-        <p className="small_text">user_123 just got warmer...</p>
-        <p className="small_text">One of you just made an amazing deal!</p>
-        <p className="small_text">11 of you did not... hurry up!</p>
       </div>
     </div>
   );
